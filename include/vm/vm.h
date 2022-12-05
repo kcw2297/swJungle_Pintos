7fb194baa901903ce01019f5acd5e20fd1f4a883
Gilin3000 <ubuntu@ip-172-31-4-224.ap-northeast-2.compute.internal>
2022-12-05 00:50:57 +0000

    feat : struct page

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -2,6 +2,7 @@
 #define VM_VM_H
 #include <stdbool.h>
 #include "threads/palloc.h"
+#include "lib/kernel/hash.h"
 
 enum vm_type {
 	/* page not initialized */
@@ -46,6 +47,10 @@ struct page {
 	struct frame *frame;   /* Back reference for frame */
 
 	/* Your implementation */
+	uint8_t type;
+	bool writable;
+	bool is_loaded;
+	struct hash_elem hash_elem; 
 
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
