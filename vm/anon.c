a0214c8da698cd6d404745ce5808750978e3bb36
Gilin3000 <ubuntu@ip-172-31-4-224.ap-northeast-2.compute.internal>
2022-12-05 10:03:03 +0000

    feat : vm_anon_init

--- vm/anon.c
+++ vm/anon.c
@@ -17,11 +17,17 @@ static const struct page_operations anon_ops = {
 	.type = VM_ANON,
 };
 
+//FOR ANON_INIT
+struct bitmap *swap_table;
+const size_t SECTORS_PER_PAGE = PGSIZE / DISK_SECTOR_SIZE;
+
 /* Initialize the data for anonymous pages */
 void
 vm_anon_init (void) {
 	/* TODO: Set up the swap_disk. */
-	swap_disk = NULL;
+	swap_disk = disk_get(1,1);
+	size_t swap_size = disk_size(swap_disk) / SECTORS_PER_PAGE;
+	swap_table = bitmap_create(swap_size);
 }
 
 /* Initialize the file mapping */
