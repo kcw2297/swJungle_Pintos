a3977d2dfa09109acb4e616cef0845f0f38fff71
Gilin3000 <ubuntu@ip-172-31-4-224.ap-northeast-2.compute.internal>
2022-12-06 00:07:30 +0000

    feature : vm_stack_growh add func

--- vm/vm.c
+++ vm/vm.c
@@ -155,6 +155,11 @@ vm_get_frame (void) {
 /* Growing the stack. */
 static void
 vm_stack_growth (void *addr UNUSED) {
+	if(vm_alloc_page(VM_ANON | VM_MARKER_0, addr, 1))
+	{
+		vm_claim_page(addr);
+		thread_current()->stack_bottom -= PGSIZE;
+	}
 }
 
 /* Handle the fault on write_protected page */
